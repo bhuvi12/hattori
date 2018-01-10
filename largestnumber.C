7f4e5c51110872700392eae29842e5b3f8890342
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-01-10 16:53:21 +0530

    THREELARGE
    LARGEST OF THREE NUMBERS

---
+++ largestnumber.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+printf("\n ENTER THE VALUE OF x,y,z");
+scanf("%d%d%d%d",&x,&y,&z);
+if((x>y)&&(x>z))
+{
+printf("\n %d is larger than%d and %d",&x&y&z);
+}
+if((y>x)&&(y>z))
+{
+printf("\n %d is larger than%d and %d",&y&x&z);
+}
+if((z>x)&&(z>b))
+{
+printf("\n %d is larger than %d ana %d",&z&y&x);
+}
+return(0);
+}
