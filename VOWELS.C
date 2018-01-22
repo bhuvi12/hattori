2f2e4dcd7dfe4c87d98858c2b973ff90df178c82
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-01-22 16:09:01 +0530

    vowels.C

---
+++ VOWELS.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main();
+{
+char ch;
+printf("enter the name:");
+scanf("%c",&ch);
+if(ch='a'//ch='e'//ch='i'//ch='o'//ch='u')
+printf("%c is a vowel:\n",ch);
+else
+printf("not a vowel",ch);
+return 0;
+}
